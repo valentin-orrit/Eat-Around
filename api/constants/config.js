f3a49c6ef8edb2e9ad84e5f03d319ee79d313865
valentin-orrit <vorrit@gmail.com>
2024-10-15 15:37:35 +0200

    fixed prisma migration failure

--- api/constants/config.js
+++ api/constants/config.js
@@ -1,6 +1,6 @@
-import pkg, { Prisma, PrismaClient } from '@prisma/client'
+import pkg from '@prisma/client';
 
-const { prismaClient } = pkg
-const prisma = new PrismaClient()
+const { PrismaClient } = pkg;
+const prisma = new PrismaClient();
 
-export default prisma
+export default prisma;
--- api/server.js
+++ api/server.js
@@ -1,44 +1,45 @@
-import express from 'express'
-import { PrismaSessionStore } from '@quixo3/prisma-session-store'
-import expressSession from 'express-session'
-import cors from 'cors'
+import express from 'express';
+import { PrismaSessionStore } from '@quixo3/prisma-session-store';
+import expressSession from 'express-session';
+import cors from 'cors';
+import prisma from './constants/config.js';
 
-const app = express()
-const PORT = 3000
+const app = express();
+const PORT = 3000;
 
 // cors setup
 app.use(
-    cors({
-        origin: ['http://localhost:5137', 'http://localhost:5174'],
-        methods: ['POST', 'PUT', 'GET', 'OPTION', 'HEAD', 'DELETE', 'PATCH'],
-        credentials: true,
-    })
-)
+  cors({
+    origin: ['http://localhost:5137', 'http://localhost:5174'],
+    methods: ['POST', 'PUT', 'GET', 'OPTION', 'HEAD', 'DELETE', 'PATCH'],
+    credentials: true,
+  })
+);
 
 // session setup
 app.use(
-    expressSession({
-        cookie: {
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'lax' : 'strict',
-            maxAge: 360000,
-            httpOnly: true,
-        },
-        secret: 'a santa at nasa',
-        resave: true,
-        saveUninitialized: true,
-        store: new PrismaSessionStore(prisma, {
-            checkPeriod: 120000,
-            dbRecordIdIsSessionId: true,
-            dbRecordIdFunction: undefined,
-        }),
-    })
-)
+  expressSession({
+    cookie: {
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: process.env.NODE_ENV === 'production' ? 'lax' : 'strict',
+      maxAge: 360000,
+      httpOnly: true,
+    },
+    secret: 'a santa at nasa',
+    resave: true,
+    saveUninitialized: true,
+    store: new PrismaSessionStore(prisma, {
+      checkPeriod: 120000,
+      dbRecordIdIsSessionId: true,
+      dbRecordIdFunction: undefined,
+    }),
+  })
+);
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(PORT, (err) => {
-    if (err) console.log(err)
-    console.log(`Server is running on port ${PORT}`)
-})
+  if (err) console.log(err);
+  console.log(`Server is running on port ${PORT}`);
+});
