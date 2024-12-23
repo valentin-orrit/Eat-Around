38788702724df9aa58b153e4233a393b64e7c0c2
valentin-orrit <vorrit@gmail.com>
2024-12-23 14:55:40 +0100

    setup shadcn + import sidebar

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+    darkMode: ['class'],
     content: ['./styles/index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
-        extend: {},
+    	extend: {
+    		borderRadius: {
+    			lg: 'var(--radius)',
+    			md: 'calc(var(--radius) - 2px)',
+    			sm: 'calc(var(--radius) - 4px)'
+    		},
+    		colors: {
+    			sidebar: {
+    				DEFAULT: 'hsl(var(--sidebar-background))',
+    				foreground: 'hsl(var(--sidebar-foreground))',
+    				primary: 'hsl(var(--sidebar-primary))',
+    				'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
+    				accent: 'hsl(var(--sidebar-accent))',
+    				'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
+    				border: 'hsl(var(--sidebar-border))',
+    				ring: 'hsl(var(--sidebar-ring))'
+    			}
+    		}
+    	}
     },
-    plugins: [],
+    plugins: [require("tailwindcss-animate")],
 }
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    resolve: {
+        alias: {
+            // eslint-disable-next-line no-undef
+            '@': path.resolve(__dirname, './src'),
+        },
+    },
+    css: {
+        preprocessorOptions: {
+            css: {
+                additionalData: '@import "tailwindcss/tailwind.css";',
+            },
+        },
+    },
     server: {
         watch: {
             usePolling: true,
