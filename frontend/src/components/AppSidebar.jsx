1b0e15be7b24fee0000f95b02382d1f0b8776e2f
valentin-orrit <vorrit@gmail.com>
2024-12-23 17:33:59 +0100

    added collapsible menu for likes

--- frontend/src/components/AppSidebar.jsx
+++ frontend/src/components/AppSidebar.jsx
@@ -1,5 +1,15 @@
 import { Link } from 'react-router-dom'
-import { Calendar, Home, Inbox, Search, Settings } from 'lucide-react'
+import {
+    Calendar,
+    Home,
+    Inbox,
+    Search,
+    Settings,
+    LogIn,
+    Gauge,
+    ChevronDown,
+    Heart,
+} from 'lucide-react'
 import {
     Sidebar,
     SidebarContent,
@@ -9,7 +19,13 @@ import {
     SidebarMenu,
     SidebarMenuButton,
     SidebarMenuItem,
+    useSidebar,
 } from '@/components/ui/sidebar'
+import {
+    Collapsible,
+    CollapsibleContent,
+    CollapsibleTrigger,
+} from '@/components/ui/collapsible'
 import {
     SignedIn,
     SignedOut,
@@ -18,62 +34,85 @@ import {
 } from '@clerk/clerk-react'
 import { useUserData } from '../hooks/useUserData'
 
-const items = [
-    {
-        title: 'Home',
-        url: '#',
-        icon: Home,
-    },
-    {
-        title: 'Inbox',
-        url: '#',
-        icon: Inbox,
-    },
-    {
-        title: 'Calendar',
-        url: '#',
-        icon: Calendar,
-    },
-    {
-        title: 'Search',
-        url: '#',
-        icon: Search,
-    },
-    {
-        title: 'Settings',
-        url: '#',
-        icon: Settings,
-    },
-]
-
 export default function AppSidebar() {
+    const { userData } = useUserData()
+
+    const {
+        state,
+        open,
+        setOpen,
+        openMobile,
+        setOpenMobile,
+        isMobile,
+        toggleSidebar,
+    } = useSidebar()
+
     return (
-        <Sidebar collapsible="icon">
+        <Sidebar collapsible="icon" className="bg-eagreen gap-y-0">
             <SidebarContent>
                 <SidebarGroup>
                     <SidebarGroupContent>
-                        <div className="flex justify-center text-white text-sm font-medium my-4">
+                        <div id="user-button" className="py-4">
                             <SignedOut>
-                                <SignInButton className="text-white bg-amber-600 hover:bg-amber-700 px-3 py-2 rounded-full " />
+                                <SidebarMenuButton asChild isActive>
+                                    <div className="flex justify-center text-white text-sm font-medium my-4">
+                                        <LogIn color="#fff" />
+                                        <span>
+                                            <SignInButton className="bg-eaorange hover:bg-amber-500 text-eaoffwhite px-3 py-1 rounded-md stroke-slate-100">
+                                                Sign In
+                                            </SignInButton>
+                                        </span>
+                                    </div>
+                                </SidebarMenuButton>
                             </SignedOut>
                             <SignedIn>
                                 <UserButton />
                             </SignedIn>
                         </div>
-                        <SidebarMenu>
-                            {items.map((item) => (
-                                <SidebarMenuItem key={item.title}>
-                                    <SidebarMenuButton asChild>
-                                        <a href={item.url}>
-                                            <item.icon />
-                                            <span>{item.title}</span>
-                                        </a>
-                                    </SidebarMenuButton>
-                                </SidebarMenuItem>
-                            ))}
-                        </SidebarMenu>
+                        <SidebarMenuButton asChild>
+                            {userData && userData.is_admin && (
+                                <Link
+                                    to="/dashboard"
+                                    className="text-eaoffwhite hover:bg-eaorange px-3 rounded-full text-sm font-medium"
+                                >
+                                    <Gauge />
+                                    Dashboard
+                                </Link>
+                            )}
+                        </SidebarMenuButton>
                     </SidebarGroupContent>
                 </SidebarGroup>
+                <Collapsible
+                    defaultOpen
+                    className="group/collapsible text-eaoffwhite "
+                >
+                    <SidebarGroup>
+                        <SidebarMenuButton
+                            asChild
+                            className="px-3 py-2 rounded-md text-sm font-medium hover:bg-eaorange "
+                        >
+                            <CollapsibleTrigger>
+                                {state === 'collapsed' ? (
+                                    <Heart
+                                        className="mr-2"
+                                        onClick={() => setOpen(true)}
+                                    />
+                                ) : (
+                                    <Heart className="mr-2" />
+                                )}
+                                Likes
+                                <ChevronDown className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180" />
+                            </CollapsibleTrigger>
+                        </SidebarMenuButton>
+                        {state === 'collapsed' ? null : (
+                            <CollapsibleContent className="text-left pl-4 ">
+                                <SidebarContent>One</SidebarContent>
+                                <SidebarContent>Two</SidebarContent>
+                                <SidebarContent>Three</SidebarContent>
+                            </CollapsibleContent>
+                        )}
+                    </SidebarGroup>
+                </Collapsible>
             </SidebarContent>
         </Sidebar>
     )
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,25 +3,18 @@ export default {
     darkMode: ['class'],
     content: ['./styles/index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
-    	extend: {
-    		borderRadius: {
-    			lg: 'var(--radius)',
-    			md: 'calc(var(--radius) - 2px)',
-    			sm: 'calc(var(--radius) - 4px)'
-    		},
-    		colors: {
-    			sidebar: {
-    				DEFAULT: 'hsl(var(--sidebar-background))',
-    				foreground: 'hsl(var(--sidebar-foreground))',
-    				primary: 'hsl(var(--sidebar-primary))',
-    				'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
-    				accent: 'hsl(var(--sidebar-accent))',
-    				'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
-    				border: 'hsl(var(--sidebar-border))',
-    				ring: 'hsl(var(--sidebar-ring))'
-    			}
-    		}
-    	}
+        extend: {
+            colors: {
+                eablack: '#26150d',
+                eabrown: '#462517',
+                eaorange: '#f9a705',
+                eagreen: '#3c8236',
+                eaoffwhite: '#fffcf2',
+                eaogreyaccent: '#a0a0a0',
+                eaogreymute: '#cccccc',
+            },
+        },
     },
-    plugins: [require("tailwindcss-animate")],
+    // eslint-disable-next-line no-undef
+    plugins: [require('tailwindcss-animate')],
 }
