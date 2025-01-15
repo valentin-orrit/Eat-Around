d4c9a393d6e3406a2919d508f90b6f5dfd09c5a3
valentin-orrit <vorrit@gmail.com>
2025-01-15 15:01:03 +0100

    moved open & closed tag onto restaurant photo

--- frontend/src/components/Map/PlaceCard.jsx
+++ frontend/src/components/Map/PlaceCard.jsx
@@ -7,21 +7,41 @@ export default function PlaceCard({ restaurant }) {
     return (
         <Card>
             <CardContent className="flex flex-col flex-wrap mt-1 p-1 w-40 sm:w-52 h-52 gap-y-1">
-                {restaurant.photos?.[0] ? (
-                    <img
-                        src={restaurant.photos[0].getUrl()}
-                        alt={restaurant.name.split(',')[0]}
-                        className="object-cover h-24 rounded-md"
-                    />
-                ) : (
-                    <div className="w-full h-auto flex items-center justify-center bg-gray-200">
-                        <span className="text-sm text-gray-600">
-                            No Image Available
-                        </span>
-                    </div>
-                )}
-                <div className="flex flex-col px-2 w-full gap-y-">
-                    <span className="text-eagreen text-start font-semibold text-nowrap text-ellipsis overflow-hidden w-full">
+                <div className="relative">
+                    {restaurant.photos?.[0] ? (
+                        <>
+                            <img
+                                src={restaurant.photos[0].getUrl()}
+                                alt={restaurant.name.split(',')[0]}
+                                className={`object-cover h-24 w-full rounded-md ${
+                                    !restaurant.opening_hours.open_now &&
+                                    'grayscale opacity-70'
+                                }`}
+                            />
+                            {restaurant.opening_hours && (
+                                <div className="absolute bottom-2 left-2">
+                                    {restaurant.opening_hours.open_now ? (
+                                        <p className="border rounded-md bg-eagreen text-eaoffwhite w-14 text-center text-xs">
+                                            open
+                                        </p>
+                                    ) : (
+                                        <p className="border rounded-md bg-red-800 text-eaoffwhite w-14 text-center text-xs">
+                                            closed
+                                        </p>
+                                    )}
+                                </div>
+                            )}
+                        </>
+                    ) : (
+                        <div className="w-full h-24 flex items-center justify-center bg-gray-200">
+                            <span className="text-sm text-gray-600">
+                                No Image Available
+                            </span>
+                        </div>
+                    )}
+                </div>
+                <div className="flex flex-col px-2 w-full gap-y-1">
+                    <span className="text-eabrown text-start font-semibold text-nowrap text-ellipsis overflow-hidden w-full">
                         {restaurant.name.split(/[,\|\-]/)[0]}
                     </span>
                     <div className="flex items-center justify-between text-xs">
@@ -44,24 +64,10 @@ export default function PlaceCard({ restaurant }) {
                             </a>
                         )}
                         {restaurant.formatted_phone_number && (
-                            <p className="text-xs tracking-tight">
+                            <p className="flex text-xs tracking-tight">
+                                <Phone size={14} className="mr-1" />
                                 {restaurant.formatted_phone_number}
                             </p>
-                        )}
-                    </div>
-                    <div className="text-xs">
-                        {restaurant.opening_hours && (
-                            <div>
-                                {restaurant.opening_hours.open_now ? (
-                                    <p className="border rounded-md bg-eagreen text-eaoffwhite w-14">
-                                        open
-                                    </p>
-                                ) : (
-                                    <p className="border rounded-md bg-red-600 text-eaoffwhite w-14">
-                                        closed
-                                    </p>
-                                )}
-                            </div>
                         )}
                     </div>
                 </div>
