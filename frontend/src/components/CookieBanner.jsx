b2b338ba7317dc31557701680dc8b486a41c1eed
Etiola <gaultiercode@gmail.com>
2025-01-17 12:16:22 +0100

    fixed gcu on homepage

--- frontend/src/components/CookieBanner.jsx
+++ frontend/src/components/CookieBanner.jsx
@@ -23,8 +23,8 @@ const CookieBanner = () => {
 
   return (
 <div className="fixed mb-4 w-3/5 bottom-0 right-0 mr-4 rounded-md bg-eagreen text-eaoffwhite p-4 shadow-lg z-50">
-  <div className="flex items-center justify-between text-start">
-    <p className="text-sm">
+  <div className="flex items-center justify-between text-start text-xs">
+    <p className="text-sm pr-8">
       This website uses cookies to enhance your browsing experience. By continuing to use this site, you acknowledge our use of cookies.
     </p>
     <div className="flex items-center space-x-4">
@@ -32,10 +32,14 @@ const CookieBanner = () => {
       <div className="pl-3">
         <Link
             to="/guc"
-            className="text-eaoffwhite hover:bg-eaorange px-3 rounded-full text-sm font-semibold"
+            className="text-eaoffwhite px-3 rounded-full text-sm font-semibold"
         >
-            <Guc />
+          <div className="lg:hidden">
+            GCU
+          </div>
+          <div className="hidden lg:block whitespace-nowrap text-xs">
             General Conditions of Use
+          </div>
         </Link>
       </div>
       <button
